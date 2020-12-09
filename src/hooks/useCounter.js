fbd586c37695f1a9f18f63f3211a7fdaa1cfb8a4
Kaj Białas <kaj.bialas@dev-industry.com>
2020-12-09 20:05:16 +0100

    issue 5 init

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,14 +1,41 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const COUNTER_ACTIONS_TYPE = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+  RESET: 'RESET',
+};
+
+const generateInitialState = (initValue) => ({
+  counterValue: initValue,
+});
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case (COUNTER_ACTIONS_TYPE.INCREMENT):
+      return {
+        counterValue: state.counterValue + 1,
+      }
+    case (COUNTER_ACTIONS_TYPE.DECREMENT):
+      return {
+        counterValue: state.counterValue - 1,
+      }
+    case (COUNTER_ACTIONS_TYPE.RESET):
+      return {
+        counterValue: 0,
+      }
+  }
+}
 
 function useCounter(counterInitialValue) {
-  const [counterValue, setCounterValue] = useState(counterInitialValue);
+  const [state, dispatch] = useReducer(counterReducer, generateInitialState(counterInitialValue));
 
-  const handleIncrement = () => setCounterValue(prevState => prevState + 1);
-  const handleDecrement = () => setCounterValue(prevState => prevState - 1);
-  const handleReset = () => setCounterValue(counterInitialValue);
+  const handleIncrement = () => dispatch({type: COUNTER_ACTIONS_TYPE.INCREMENT});
+  const handleDecrement = () => dispatch({type: COUNTER_ACTIONS_TYPE.DECREMENT});
+  const handleReset = () => dispatch({type: COUNTER_ACTIONS_TYPE.RESET});
 
   return {
-    counterValue,
+    counterValue: state.counterValue,
     handleIncrement,
     handleDecrement,
     handleReset,
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,15 +2,21 @@ import { useState, useEffect } from 'react';
 
 function useFetch(apiEndpoint, initData) {
   const [ data, setData ] = useState(initData);
+  const [ loading, setLoading ] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch(apiEndpoint)
       .then(response => response.json())
-      .then(response => setData(response));
+      .then(response => {
+        setLoading(false);
+        setData(response);
+      });
   }, []);
 
   return {
     data,
+    loading,
   }
 }
 
